07488f4cc930c3b6e3d74281017435532eb2dc00
Fred <fred.gerner@nab.com.au>
2020-06-10 10:55:17 +1000

    added gift component

---
+++ src/components/Gift.test.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {shallow} from 'enzyme';
+import toJson from "enzyme-to-json";
+import Gift from "./Gift";
+
+describe('Gift', () => {
+    const gift = shallow(<Gift/>);
+
+    it('renders properly', function () {
+        expect(toJson(gift)).toMatchSnapshot();
+    });
+
+    it('initializes a person and present in `state`', () => {
+        expect(gift.state()).toEqual({person:'', gift: ''})
+    });
+
+    describe('When typing into the person input', () => {
+        const person = 'Wife';
+
+        beforeEach(() => {
+            gift.find('.input-person').simulate('change', {target: {value: person}});
+        });
+        it('should update the person in state', function () {
+            expect(gift.state().person).toEqual(person);
+        });
+    });
+});
+
+
