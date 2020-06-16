038e89e27a639a548a42b86c99d2d2e1fd403410
Fred <fred.gerner@nab.com.au>
2020-06-16 16:16:39 +1000

    added remove gift functionality

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,11 +11,13 @@ describe('App component', () => {
     it('renders correctly', () => {
         expect(toJson(app)).toMatchSnapshot();
     })
-
     it('initializes the `state` with an empty list of gifts', () => {
         expect(app.state().gifts).toEqual([]);
     });
+
     describe('when clicking the `add gift` button', () => {
+        const id = 1;
+
         beforeEach(() => {
             app.find('.btn-add').simulate('click')
         });
@@ -25,18 +27,23 @@ describe('App component', () => {
                 {gifts: []}
             )
         })
-
         it('adds new gift to `state`', () => {
-            expect(app.state().gifts).toEqual([{id: 1}]);
+            expect(app.state().gifts).toEqual([{id}]);
         });
-
         it('adds a new gift to the rendered list', () => {
             expect(app.find('.gift-list').children().length).toEqual(1);
         });
-
         it('creates a gift component', () => {
             expect(app.find('Gift').exists()).toBe(true);
-        })
+        });
+        describe('and the user wants to remove the added gift', () => {
+            beforeEach(() => {
+                app.instance().removeGift(id);
+            });
 
+            it('removes gift from `state`', () => {
+                expect(app.state().gifts).toEqual([]);
+            });
+        });
     })
 })
--- src/components/Gift.test.js
+++ src/components/Gift.test.js
@@ -4,14 +4,17 @@ import toJson from "enzyme-to-json";
 import Gift from "./Gift";
 
 describe('Gift', () => {
-    const gift = shallow(<Gift/>);
+    const mockRemove = jest.fn();
+    const id = 1;
+    const props = {gift: {id}, removeGift: mockRemove};
+    const gift = shallow(<Gift {...props} />);
 
     it('renders properly', function () {
         expect(toJson(gift)).toMatchSnapshot();
     });
 
     it('initializes a person and present in `state`', () => {
-        expect(gift.state()).toEqual({person:'', gift: ''})
+        expect(gift.state()).toEqual({person: '', gift: ''})
     });
 
     describe('When typing into the person input', () => {
@@ -27,14 +30,24 @@ describe('Gift', () => {
 
     describe('when typing into the present input', () => {
         const present = 'Drone';
-        
+
         beforeEach(() => {
             gift.find('.input-present').simulate('change', {target: {value: present}});
         })
         it('updates the present in `state`', () => {
             expect(gift.state().present).toEqual(present);
         })
-    })
+    });
+
+    describe('when clicking the `remove gift` button', () => {
+        beforeEach(() => {
+            gift.find('.btn-remove').simulate('click');
+        });
+
+        it('calls the remove gift callback', () => {
+            expect(mockRemove).toHaveBeenCalledWith(id);
+        });
+    });
 });
 
 
